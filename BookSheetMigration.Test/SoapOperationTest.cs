f0097a3a2464c76238cf25a315b0fc46e3323cd6
coderasm <coderasm84k@gmail.com>
2015-04-29 17:43:24 -0700

    Added unit tests for soap xml string request generator and added unit tests for soap operation

---
+++ BookSheetMigration.Test/SoapOperationTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.Remoting;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookSheetMigration;
+
+namespace BookSheetMigration.Test
+{
+    [TestClass]
+    public class SoapOperationTest
+    {
+
+        private SoapOperation soapOperation;
+
+        [TestInitialize]
+        public void setUp()
+        {
+            String operation = "operation";
+            soapOperation = new SoapOperation(operation);
+        }
+
+        [TestMethod]
+        public void WhenAddingOnePairToOperation_ThenPairCountIsOne()
+        {
+            var key = "akey";
+            var value = "avalue";
+            soapOperation.addPairToOperation(key, value);
+            Assert.AreEqual(1, soapOperation.getPairCount());
+        }
+    }
+}
---
+++ BookSheetMigration.Test/SoapXMLRequestStringGeneratorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookSheetMigration.Test
+{
+    [TestClass]
+    public class SoapXMLRequestStringGeneratorTest
+    {
+        [TestMethod]
+        public void WhenGivenASoapOperation_AnXMLStringIsBuilt()
+        {
+            SoapOperation soapOperation = new SoapOperation("anoperation");
+            soapOperation.addPairToOperation("password", "mypassword");
+            soapOperation.addPairToOperation("username", "myusername");
+            soapOperation.addPairToOperation("securitytoken", "mysecuritytoken");
+            String soapXMLStringExpected = @"<?xml version=""1.0"" encoding=""utf-8""?>" +
+                                            @"<soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""" +
+                                            @"xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">" +
+                                                "<soap:Body>";
+            SoapXMLRequestStringGenerator soapXmlRequestStringGenerator = new SoapXMLRequestStringGenerator(soapOperation);
+            String soapRequestString = soapXmlRequestStringGenerator.generate();
+        }
+    }
+}
