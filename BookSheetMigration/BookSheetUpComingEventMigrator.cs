ea61dd710a6cded4d97ef1319c35479b34738989
coderasm <coderasm84k@gmail.com>
2015-05-12 18:10:23 -0700

    Forgot to add these files

---
+++ BookSheetMigration/BookSheetUpComingEventMigrator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookSheetMigration
+{
+    class BookSheetUpComingEventMigrator : DataMigrator<AWGEventDTO>
+    {
+        private EventStatus eventStatus;
+
+        public BookSheetUpComingEventMigrator(EventStatus eventStatus)
+        {
+            this.eventStatus = eventStatus;
+        }
+
+        protected override List<AWGEventDTO> findPossiblyNewRecords()
+        {
+            var awgServiceClient = new AWGServiceClient();
+            var eventDirectory = awgServiceClient.findEventsByStatus(eventStatus);
+            this.possiblyNewRecords = eventDirectory.awgEvents;
+        }
+
+        protected override List<AWGEventDTO> findAlreadyMigratedRecords(List<AWGEventDTO> foundPossiblyNewRecords)
+        {
+            var bookSheetEventDao = new AbsBookSheetEventDAO();
+            return bookSheetEventDao.FindEventsIn(foundPossiblyNewRecords).Result;
+        }
+
+        protected override void migrateRecord(AWGEventDTO possiblyNewRecord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookSheetMigration/DataMigrator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSheetMigration
+{
+    public abstract class DataMigrator<T>
+    {
+        protected List<T> existingRecords;
+        protected List<T> possiblyNewRecords;
+
+        protected DataMigrator()
+        {
+            possiblyNewRecords = findPossiblyNewRecords();
+            existingRecords = findExistingRecords(possiblyNewRecords);
+        }
+
+        protected abstract List<T> findPossiblyNewRecords();
+
+        private List<T> findExistingRecords(List<T> foundPossiblyNewRecords)
+        {
+            if(possibleRecordsToMigrateExist(foundPossiblyNewRecords))
+                return findAlreadyMigratedRecords(foundPossiblyNewRecords);
+            else
+                return new List<T>();
+        }
+
+        private bool possibleRecordsToMigrateExist(List<T> foundPossiblyNewRecords)
+        {
+ 	        return foundPossiblyNewRecords.Count > 0;
+        }
+
+        protected abstract List<T> findAlreadyMigratedRecords(List<T> foundPossiblyNewRecords);
+
+        public void migrateRecords()
+        {
+            foreach (var possiblyNewRecord in possiblyNewRecords)
+            {
+                if (shouldMigrateRecord(possiblyNewRecord))
+                    migrateRecord(possiblyNewRecord);
+            }
+        }
+
+        protected abstract void migrateRecord(T possiblyNewRecord);
+
+        private bool shouldMigrateRecord(T possiblyNewRecord)
+        {
+            return !existingRecords.Contains(possiblyNewRecord);
+        }
+    }
+}
