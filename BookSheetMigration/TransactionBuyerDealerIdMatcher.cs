e3230c64c2116b1c6aa3779260067702a6d015b0
coderasm <coderasm84k@gmail.com>
2015-05-26 15:13:49 -0700

    Added buyer dealer id matcher for transactions

---
+++ BookSheetMigration/TransactionBuyerDealerIdMatcher.cs
@@ -0,0 +1,27 @@
+﻿namespace BookSheetMigration
+{
+    class TransactionBuyerDealerIdMatcher : DealerIdMatcher
+    {
+
+        public TransactionBuyerDealerIdMatcher(AWGTransactionDTO transaction)
+        {
+            this.transaction = transaction;
+            query = dealerQuery;
+        }
+
+        protected override bool entityNumberExists()
+        {
+            return transaction.buyerNumber != "";
+        }
+
+        protected override string getEntityNumber()
+        {
+            return transaction.buyerNumber;
+        }
+
+        protected override void setEntityId(DealerDTO entity)
+        {
+            transaction.buyerDealerId = entity.dealerId;
+        }
+    }
+}
